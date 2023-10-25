18c7ab75bf7f15aa32564635faa1ea1d5032dbdc
Philippe Volte--Vieira <philippe.volte-vieira@universite-paris-saclay.fr>
2023-10-25 20:18:23 +0200

    discriminate impure expressions

--- src/test_ref.ml
+++ src/test_ref.ml
@@ -11,3 +11,8 @@ let f2 = fun x -> (x + x)
 let f3 = fun x -> !x
 
 let f4 = fun (x:Ref) -> x
+
+let f5 = fun f -> if (f 5) is Int then (f 5) else 2
+(* no_se : ((5 -> Int & 'a) -> Int & 'a) & ((5 -> 'b) -> 2 | Int & 'b)
+   se    : ((5 -> 'a) -> 2 | 'a) & ((5 -> Int & 'b) -> Int & 'b) &
+           ((5 -> Any \ Int) -> 2) *)
