0b7e812e8620f07cade6339ee7490d1b806c73a8
Philippe Volte--Vieira <philippe.volte-vieira@universite-paris-saclay.fr>
2023-11-02 15:06:02 +0100

    tests & verifs

--- src/main/prototype.ml
+++ src/main/prototype.ml
@@ -16,8 +16,12 @@ let () =
            match type_check_def tenv env (v,e,ta) with
            | TSuccess (t, env, (tmsc, ttype)) ->
               Format.printf
-                "%s (checked in %.02fms (msc:%.02fms, type:%.02fms))\n%!"
-                (Types.Tvar.string_of_type_short t) (tmsc +. ttype) tmsc ttype ;
+                "%s (checked in %.02fms (msc:%.02fms, type:%.02fms), \
+                 pure:@{<green>%s@}@{<red>%s@})\n%!"
+                (Types.Tvar.string_of_type_short t)
+                (tmsc +. ttype) tmsc ttype
+                (if Parsing.Ast.is_pure e then "yes" else "")
+                (if Parsing.Ast.is_pure e |> not then "no" else "");
               env
            | TFailure (pos, msg, _) ->
               Format.printf "Ill typed\n%!" ;
--- src/test_ref.ml
+++ src/test_ref.ml
@@ -1,18 +1,33 @@
-let my_ref = ref 5
+let const = 5
 
-let inside = !my_ref
+let alloc = ref 5
 
-let _ = (my_ref := true)
+let read = !alloc
 
-let f1 = fun x -> succ x
+let assign = (alloc := true)
+
+let f0 = fun x -> read (* pure *)
+
+let f1 = fun x -> succ x (* pure *)
 
 let f2 = fun x -> (x + x)
 
 let f3 = fun x -> !x
 
-let f4 = fun (x:Ref) -> x
+let f4 = fun (x:Ref) -> x (* pure *)
 
-let f5 = fun f -> if (f 5) is Int then (f 5) else 2
-(* no_se : ((5 -> Int & 'a) -> Int & 'a) & ((5 -> 'b) -> 2 | Int & 'b)
-   se    : ((5 -> 'a) -> 2 | 'a) & ((5 -> Int & 'b) -> Int & 'b) &
-           ((5 -> Any \ Int) -> 2) *)
+let f5 = fun f -> if (f 5) is Int
+                  then (f 5)
+                  else 2
+(*
+  no_se : ((5 -> Int & 'a) -> Int & 'a)
+          & ((5 -> 'b) -> 2 | Int & 'b)
+
+   se    :  ((5 -> 'a) -> 2 | 'a)
+            & ((5 -> Int & 'b) -> Int & 'b)
+            & ((5 -> Any \ Int) -> 2)
+*)
+
+let f6 = fun x -> if (f2 (succ x)) is 42
+                  then (f2 (succ x))
+                  else true
